4b83d2f044391571bc59ba20817d7c936ca12018
Sagar Pal <57063071+sagar510@users.noreply.github.com>
2026-07-28 02:01:32 +0530

    Time: 6 ms (97%), Space: 47.6 MB (75.6%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,50 +9,16 @@
  */
 
 class Solution {
-    class TreeInfo{
-        TreeNode ans;
-        boolean pFound;
-        boolean qFound;
-
-        TreeInfo(TreeNode ans, boolean pFound, boolean qFound){
-            this.ans = ans;
-            this.pFound = pFound;
-            this.qFound = qFound;
-        }
-    }
-
-    public TreeInfo dfs(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return new TreeInfo(null, false, false);
-
-        TreeInfo left = dfs(root.left, p, q);
-        if(left.ans != null) return new TreeInfo(left.ans, true, true);
-
-        TreeInfo right = dfs(root.right, p, q);
-        if(right.ans != null) return new TreeInfo(right.ans, true, true);
-
-        if((left.pFound||right.pFound) && (left.qFound||right.qFound)){
-            return new TreeInfo(root, true, true);
-        }
-
-        if(root==p){
-            if(left.qFound || right.qFound){
-                return new TreeInfo(root, true, true);
-            }
-            return new TreeInfo(null, true, false);
-        }
-
-        if(root==q){
-            if(left.pFound || right.pFound){
-                return new TreeInfo(root, true, true);
-            }
-            return new TreeInfo(null, false, true);
-        }
-
-        return new TreeInfo(null, left.pFound || right.pFound, left.qFound || right.qFound);
-    }
-
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return dfs(root,p,q).ans;
+        if(root == null) return null;
+        if(root==p || root==q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left!=null && right!=null) return root;
+        if(left!=null && right==null) return left;
+        if(right!=null && left==null) return right;
+        return null;
     }
 }
