081a49fc1e0009adf8efd448ba347927318b3e30
Sagar Pal <57063071+sagar510@users.noreply.github.com>
2026-07-29 17:07:32 +0530

    Time: 0 ms (100%), Space: 47.4 MB (49.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int sum = n*(n+1)/2;
-        int actual = 0;
-        for(int num : nums) actual += num;
-        return sum - actual;
+        int missing = nums.length;
+        for(int i=0; i<nums.length; i++){
+            missing += i - nums[i];
+        }
+        return missing;
     }
 }
